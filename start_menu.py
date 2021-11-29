2ff3cffc322d838781d74cbb3b8106d2f4bbe0c1
jamie <jamietohhl@gmail.com>
2021-11-29 17:20:05 +0800

    Created function start_menu()

---
+++ start_menu.py
@@ -0,0 +1,27 @@
+# display start menu function
+def start_menu():
+    # stores the valid options that users can enter
+    menu_options = ["0", "1", "2"]
+    while (True):
+        
+        print("Welcome, mayor of Simp City!")
+        print("----------------------------")
+        print("1. Start new game")
+        print("2. Load saved game\n")
+        print("0. Exit")
+        option = input("Your choice? ")
+        if option in menu_options:
+            if option == "1":
+                #start_game()
+                print("start game") #placeholder text
+            elif option == "2":
+                #load_saved_game()
+                print("load saved game") #placeholder text
+            else:
+                print("Exiting Simp City...")
+                break
+        else:
+            print("Invalid option! Please try again!\n")
+    return
+
+start_menu()
