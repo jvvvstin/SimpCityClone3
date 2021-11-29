e43891e975b89ec71bdcad05ff02c621b2c28c1d
rhys chua <srhyschua@gmail.com>
2021-11-29 17:09:22 +0800

    Created function start_new_game

---
+++ start_new_game.py
@@ -0,0 +1,11 @@
+#this function starts a new game
+#create a new board to show to user
+#create variable for turn number
+def start_new_game():
+    board = [["?","?","?","?"],
+             ["?","?","?","?"],
+             ["?","?","?","?"],
+             ["?","?","?","?"]]
+    turn_num = 1
+    #display_board(turn_num, board)
+    return
