fd758cc83c6b5c030d4dff7bdcc061dfc7e153a2
Qi Xun <qixunnn@gmail.com>
2021-12-02 00:14:51 +0800

    Created display_board() function

---
+++ display_board.py
@@ -0,0 +1,21 @@
+# Function to display the board to user
+# Takes in the turn number, and the board to display
+def display_board(turn_num, board):
+
+    print("\nTurn " + str(turn_num))
+    print("     A     B     C     D   ")
+    print("  +-----+-----+-----+-----+")
+    for i in range(len(board)):
+        print(" " + str(i+1) + "|", end="")
+        for j in range(len(board[i])):
+            if (board[i][j] != "?"):
+
+                print(" " + board[i][j] + " |", end="")
+            else:
+                print("     |", end="")
+        print("\n  +-----+-----+-----+-----+")
+    return
+
+
+display_board(3, [["?", "?", "?", "?"], ["?", "?", "?", "?"],
+              ["?", "?", "?", "?"], ["?", "?", "?", "?"]])
